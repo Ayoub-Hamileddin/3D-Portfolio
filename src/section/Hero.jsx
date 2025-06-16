dedf4cdc223c8def4493607b21f8969611047d0c
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2025-06-16 22:46:04 +0100

    add the Astronut components in the hero section

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 import HeroText from "../components/HeroText";
 import ParallaxBackground from "../components/ParallaxBackground";
+import { Canvas } from "@react-three/fiber";
+import { Astronut } from "../components/Astronut";
+import { OrbitControls } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853 });
   return (
     <div className="flex items-start justify-center min-h-screen overflow-hidden md:items-start md:justify-start c-space ">
       <HeroText />
       <ParallaxBackground />
+      <figure
+        className="absolute inset-0"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas camera={{ position: [0, 1, 3] }}>
+          <Astronut
+            scale={isMobile && 0.23}
+            position={isMobile && [0, -1.5, 0]}
+          />
+          <OrbitControls />
+        </Canvas>
+      </figure>
     </div>
   );
 };
